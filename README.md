582692b53a10e37c75e6242a1fec4755a7ebd485
冀金梁 <625103471@qq.com>
2023-10-07 14:59:12 +0800

    changes

---
+++ README.md
@@ -0,0 +1,11 @@
+Zookeeper入门
+
+1.依赖
+
+    <dependency>
+        <groupId>org.apache.zookeeper</groupId>
+        <artifactId>zookeeper</artifactId>
+        <version>3.4.9</version>
+    </dependency>
+
+2.
---
+++ src/main/java/com/kyrie/MyZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.kyrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyZookeeperApplication.class, args);
+    }
+
+}
